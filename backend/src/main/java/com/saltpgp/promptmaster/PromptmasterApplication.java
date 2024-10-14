0feb836a5d5ba2a7f7a4dfba12789a9bf9f9d183
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-14 02:08:44 +0200

    Setting up project folders

---
+++ backend/src/main/java/com/saltpgp/promptmaster/PromptmasterApplication.java
@@ -0,0 +1,13 @@
+package com.saltpgp.promptmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptmasterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/saltpgp/promptmaster/PromptmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saltpgp.promptmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
