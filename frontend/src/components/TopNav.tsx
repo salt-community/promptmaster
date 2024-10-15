3d82d1ec25539aa20a860412e01c4e23f303f297
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-15 04:25:03 +0200

    Creating Top Navbar

---
+++ frontend/src/components/TopNav.tsx
@@ -0,0 +1,17 @@
+function TopNav() {
+  return (
+    <>
+      <div className="navbar bg-custom-secondary pt-5 ">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl text-white">Salt</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn bg-white text-black">
+            Contact Us
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default TopNav;
