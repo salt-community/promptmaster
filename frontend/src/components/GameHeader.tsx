a84a34fb2f39ebba1241b2cb5e730c10180316b5
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-15 05:02:45 +0200

    Adding GameHeader

---
+++ frontend/src/components/GameHeader.tsx
@@ -0,0 +1,22 @@
+function GameHeader() {
+  return (
+    <>
+      <div className="hero bg-base-200 ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Prompt Master</h1>
+            <p className="py-6">
+            Welcome to Prompt Master, the ultimate game where creativity meets challenge! Can you craft the perfect prompt to recreate this stunning image? Test your skills and become the master of imagination!
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default GameHeader;
