97b6a88eaf93bf8a99b5c79744ae0c4755052afd
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-21 01:54:48 +0200

    Prop drilling

--- frontend/src/components/ImageGenerator.tsx
+++ frontend/src/components/ImageGenerator.tsx
@@ -1,16 +1,36 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import image from "../assets/empty.png";
 import loadingGif from "../assets/Cube@1x-1.0s-200px-200px.gif";
 
-
 type Props = {
-    setScore: React.Dispatch<React.SetStateAction<string>>;
-    targetImageUrl: string;
-}
+  setScore: React.Dispatch<React.SetStateAction<string>>;
+  targetImageUrl: string;
+  playerName: string;
+  setPlayerName: React.Dispatch<React.SetStateAction<string>>;
+  playerPhone: string;
+  setPlayerPhone: React.Dispatch<React.SetStateAction<string>>;
+  userInfoEntered: boolean;
+  setUserInfoEntered: React.Dispatch<React.SetStateAction<boolean>>;
+  imageUrl: string;
+  setImageUrl: React.Dispatch<React.SetStateAction<string>>;
+};
 
-function ImageGenerator({setScore,targetImageUrl}:Props) {
-  const [imageUrl, setImageUrl] = useState<string>("");
-  let imageUrl2="";
+function ImageGenerator({
+  setScore,
+  targetImageUrl,
+  playerName,
+  setPlayerName,
+  playerPhone,
+  setPlayerPhone,
+  userInfoEntered,
+  setUserInfoEntered,
+  imageUrl,
+  setImageUrl,
+}: Props) {
+  // const [name, setName] = useState('');
+  // const [phone, setPhone] = useState('');
+  // const [imageUrl, setImageUrl] = useState<string>("");
+  let imageUrl2 = "";
   const [prompt, setPrompt] = useState<string>("");
   const API_KEY = import.meta.env.VITE_API_KEY;
   const [fetchError, setfetchError] = useState(false);
@@ -41,10 +61,11 @@ function ImageGenerator({setScore,targetImageUrl}:Props) {
       })
       .then((data) => {
         setImageUrl(data?.data[0]?.url);
-        imageUrl2=data?.data[0]?.url;
+        imageUrl2 = data?.data[0]?.url;
         console.log(data?.data[0]?.url);
         console.log(imageUrl);
-      }).then(()=> console.log("iam here"))
+      })
+      .then(() => console.log("iam here"))
       .catch((e) => {
         setScore("");
         setfetchError(true);
@@ -63,29 +84,28 @@ function ImageGenerator({setScore,targetImageUrl}:Props) {
       model: "gpt-4o-mini",
       messages: [
         {
-          "role": "user",
-          "content": [
+          role: "user",
+          content: [
             {
-              "type": "text",
-              "text": "Compare these two images and assign a score in percentage on how the objects in the image are related using semantic similarity (i.e., whether the images depict similar objects).Do not give 0 as an answer. Reply as a number with just the score as a value between 1 and 100"
+              type: "text",
+              text: "Compare these two images and assign a score in percentage on how the objects in the image are related using semantic similarity (i.e., whether the images depict similar objects).Do not give 0 as an answer and give scores above 85 only if the similarity is really close. Reply as a number with just the score as a value between 1 and 100",
             },
             {
-              "type": "image_url",
-              "image_url": {
-                "url": targetImageUrl,
-              }
+              type: "image_url",
+              image_url: {
+                url: targetImageUrl,
+              },
             },
             {
-              "type": "image_url",
-              "image_url": {
-                "url": imageUrl2,
-              }
-            }
-          ]
-        }
+              type: "image_url",
+              image_url: {
+                url: imageUrl2,
+              },
+            },
+          ],
+        },
       ],
-      "max_tokens": 300,
-
+      max_tokens: 300,
     };
     await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
@@ -103,7 +123,9 @@ function ImageGenerator({setScore,targetImageUrl}:Props) {
       })
       .then((data) => {
         console.log(data);
-        setScore(data?.choices[0]?.message?.content);
+        setTimeout(() => {
+          setScore(data?.choices[0]?.message?.content);
+        }, 2000);
       })
       .catch((e) => {
         setScore("");
@@ -115,11 +137,12 @@ function ImageGenerator({setScore,targetImageUrl}:Props) {
       });
   }
 
-   async function handleClick() {
+  async function handleClick() {
     console.log("hi");
     setScore("");
+    setPrompt("");
     await sendPromptToOpenAI();
-    console.log("prompt is done")
+    console.log("prompt is done");
     await sendimagestoOpenAI();
     console.log("image comparison is done");
   }
@@ -137,11 +160,63 @@ function ImageGenerator({setScore,targetImageUrl}:Props) {
     }
   }, [fetchError]);
 
+  function handleUserInfoSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (playerName && playerPhone) {
+      setUserInfoEntered(true);
+    }
+  }
+
   return (
-    <>
-      {/* <div className="flex flex-col items-center justify-center  bg-custom-tertiary p-6"> */}
-        <div className=" w-1/3  bg-custom-primary rounded-lg shadow-lg p-6 mb-6">
-          <div className=" flex items-center justify-center mb-4">
+    <div className="w-1/3 bg-custom-primary rounded-lg shadow-lg p-6 mb-6">
+      {!userInfoEntered ? (
+        <>
+          {" "}
+          <div className="flex justify-center h-full items-center bg-slate-300 rounded-md">
+            <form onSubmit={handleUserInfoSubmit} >
+              {/* <p className="text-center text-lg font-semibold text-gray-800 mb-5">Enter your name and number to start playing</p> */}
+              <div className="mb-4">
+                <label className="block text-gray-700 text-lg font-bold mb-2">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  value={playerName}
+                  onChange={(e) => setPlayerName(e.target.value)}
+                  placeholder="Enter your name"
+                  className="bg-white text-gray-700 text-lg px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-custom-secondary placeholder-gray-500 w-full"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label className="block text-gray-700 text-lg font-bold mb-2">
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                  name="phone"
+                  value={playerPhone}
+                  onChange={(e) => setPlayerPhone(e.target.value)}
+                  placeholder="Enter your phone number"
+                  className="bg-white text-gray-700 text-lg px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-custom-secondary placeholder-gray-500 w-full"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="bg-custom-secondary hover:bg-orange-300 text-white font-bold px-6 py-2 rounded-full transition-all duration-300"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <>
+          {/* <div className="flex flex-col items-center justify-center  bg-custom-tertiary p-6"> */}
+          {/* <div className="w-1/3 bg-custom-primary rounded-lg shadow-lg p-6 mb-6"> */}
+          <div className="flex items-center justify-center mb-4">
             {isLoading ? (
               <img src={loadingGif} alt="Loading..." className="w-full" />
             ) : (
@@ -159,7 +234,7 @@ function ImageGenerator({setScore,targetImageUrl}:Props) {
               name="prompt"
               value={prompt}
               onChange={handleChange}
-              className=" bg-white text-gray-700 text-lg px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-custom-secondary placeholder-gray-500 overflow-ellipsis"
+              className="bg-white text-gray-700 text-lg px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-custom-secondary placeholder-gray-500 overflow-ellipsis"
             />
             <button
               className="bg-custom-secondary hover:bg-orange-300 text-white font-bold px-6 py-2 rounded-full ml-4 transition-all duration-300"
@@ -169,15 +244,15 @@ function ImageGenerator({setScore,targetImageUrl}:Props) {
             </button>
           </div>
           {fetchError && (
-          <p className="text-red-500 text-sm break-words whitespace-normal flex justify-center items-center text-center">
-            {" "}
-            {`Sorry,Please try again later.${fetchErrorLog}`}
-          </p>
-        )}
-        </div>
-       
-      {/* </div> */}
-    </>
+            <p className="text-red-500 text-sm break-words whitespace-normal flex justify-center items-center text-center">
+              {`Sorry,Please try again later.${fetchErrorLog}`}
+            </p>
+          )}
+          {/* </div> */}
+          {/* </div> */}
+        </>
+      )}
+    </div>
   );
 }
 
