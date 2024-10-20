5274ce30cde78ad47d72a86fbe7caccf8e71c2a3
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-21 01:50:05 +0200

    Css Changes

--- frontend/src/components/TopNav.tsx
+++ frontend/src/components/TopNav.tsx
@@ -1,12 +1,13 @@
 function TopNav() {
   return (
     <>
-      <div className="navbar bg-custom-secondary p-5 ">
-        <div className="flex-1">
+      <div className="navbar bg-custom-secondary p-5 flex justify-between">
+        <div className="justify-start">
           <a  href="https://salt.dev/" target="_blank" rel="noopener noreferrer" className="btn btn-ghost text-xl text-white">{"</salt>"}</a>
         </div>
-        <div className="flex-none">
-          <a href="https://salt.dev/contact/" target="_blank" rel="noopener noreferrer"><button className="btn bg-white text-black">
+        <div><p className="text-2xl text-white font-bold">Prompt Master</p></div>
+        <div className="">
+          <a href="https://salt.dev/contact/" target="_blank" rel="noopener noreferrer"><button className="btn bg-white hover:bg-custom-tertiary hover:text-white hover:border-custom-tertiary hover:scale-x-105 ">
             Contact Us
           </button></a>
           
