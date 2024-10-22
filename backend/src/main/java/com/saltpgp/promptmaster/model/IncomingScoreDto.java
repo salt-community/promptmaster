a61da93cc33e04f03b7dc0129e3652082fcd36c6
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-22 03:40:10 +0200

    Updating to Score model to have prompt and imageUrl

--- backend/src/main/java/com/saltpgp/promptmaster/model/IncomingScoreDto.java
+++ backend/src/main/java/com/saltpgp/promptmaster/model/IncomingScoreDto.java
@@ -1,4 +1,4 @@
 package com.saltpgp.promptmaster.model;
 
-public record IncomingScoreDto(String name, long score, String phone) {
+public record IncomingScoreDto(String name, long score, String phone, String prompt, String base64) {
 }
--- backend/src/main/java/com/saltpgp/promptmaster/model/ScoreBoard.java
+++ backend/src/main/java/com/saltpgp/promptmaster/model/ScoreBoard.java
@@ -25,12 +25,26 @@ public class ScoreBoard {
     @Column
     private String phone;
 
+    @Column
+    private String prompt;
+
+    @Column
+    private String imageurl;
+
+    @Column(columnDefinition = "TEXT")
+    private String base64;
+
+
     public ScoreBoard() {
 
     }
-    public ScoreBoard(String name, long score, String phone) {
+
+    public ScoreBoard(String name, long score, String phone, String prompt, String imageurl, String base64) {
         this.name = name;
         this.score = score;
         this.phone = phone;
+        this.prompt = prompt;
+        this.imageurl = imageurl;
+        this.base64 = base64;
     }
 }
