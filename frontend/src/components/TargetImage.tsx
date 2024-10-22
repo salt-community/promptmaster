787c439c8b586c4fe56a72039d9aa8cdc9bfdaab
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-22 03:44:19 +0200

    Css changes

--- frontend/src/components/TargetImage.tsx
+++ frontend/src/components/TargetImage.tsx
@@ -9,96 +9,96 @@ type Props = {
 
 function TargetImage({ targetImageUrl, setTargetImageUrl }: Props) {
   //   const [imageUrl, setImageUrl] = useState<string>("");
-  const API_KEY = import.meta.env.VITE_API_KEY;
-  const [fetchError, setfetchError] = useState(false);
-  const [fetchErrorLog, setfetchErrorLog] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  // const API_KEY = import.meta.env.VITE_API_KEY;
+  // const [fetchError, setfetchError] = useState(false);
+  // const [fetchErrorLog, setfetchErrorLog] = useState("");
+   const [isLoading, setIsLoading] = useState(false);
 
-  async function sendPromptToOpenAI() {
-    setIsLoading(true);
-    const apiRequestBody = {
-      model: "dall-e-3",
-      prompt: "a random image that is hard to generate",
-      n: 1,
-      size: "1024x1024",
-    };
-    await fetch("https://api.openai.com/v1/images/generations", {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(apiRequestBody),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw Error;
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setTargetImageUrl(data?.data[0]?.url);
-        console.log(data?.data[0]?.url);
-        console.log(targetImageUrl);
-      })
-      .catch((e) => {
-        setfetchError(true);
-        setfetchErrorLog(e.message);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }
+  // async function sendPromptToOpenAI() {
+  //   setIsLoading(true);
+  //   const apiRequestBody = {
+  //     model: "dall-e-3",
+  //     prompt: "a random image that is hard to generate",
+  //     n: 1,
+  //     size: "1024x1024",
+  //   };
+  //   await fetch("https://api.openai.com/v1/images/generations", {
+  //     method: "POST",
+  //     headers: {
+  //       Authorization: "Bearer " + API_KEY,
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(apiRequestBody),
+  //   })
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw Error;
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       setTargetImageUrl(data?.data[0]?.url);
+  //       console.log(data?.data[0]?.url);
+  //       console.log(targetImageUrl);
+  //     })
+  //     .catch((e) => {
+  //       setfetchError(true);
+  //       setfetchErrorLog(e.message);
+  //     })
+  //     .finally(() => {
+  //       setIsLoading(false);
+  //     });
+  // }
 
-  function handleClick() {
-    console.log("hi");
-    sendPromptToOpenAI();
-  }
+  // function handleClick() {
+  //   console.log("hi");
+  //   sendPromptToOpenAI();
+  // }
 
 
-  useEffect(() => {
-    if (fetchError) {
-      setTimeout(() => {
-        setfetchError(false);
-      }, 2000);
-    }
-  }, [fetchError]);
+  // useEffect(() => {
+  //   if (fetchError) {
+  //     setTimeout(() => {
+  //       setfetchError(false);
+  //     }, 2000);
+  //   }
+  // }, [fetchError]);
 
   return (
     <>
       {/* <div className="flex flex-col items-center justify-center  bg-custom-tertiary p-6"> */}
-      <div className="flex-col w-1/3 bg-custom-primary rounded-lg shadow-lg p-6 mb-6">
+      <div className="flex-col w-[35%] bg-custom-primary rounded-lg shadow-lg p-6 mb-6 ">
         <div className=" flex items-center justify-center mb-4">
           {isLoading ? (
             <img src={loadingGif} alt="Loading..." className="w-full" />
           ) : (
             <img
-              src={targetImageUrl === "" ? image : targetImageUrl}
+              src={targetImageUrl === "" ? image : `data:image/png;base64,${targetImageUrl}`}
               alt="Generated"
               className="w-full rounded-md"
             />
           )}
         </div>
-        <div className="flex-col p-1 place-self-center">
-          <p className="text-center text-lg font-semibold text-gray-800  border-l-4 rounded-md">
+        <div className="flex justify-stretch items-center">
+          <p className="text-center text-lg font-semibold text-gray-800  border-l-4 rounded-md ">
             Can You Match This Masterpiece? Let’s See Your Skills!
           </p>
 
-          <div className="flex justify-center items-center pt-2">
+          {/* <div className="flex justify-center items-center pt-2">
             <button
               className=" bg-custom-secondary hover:bg-orange-300 text-white font-bold px-6 py-2 rounded-full transition-all duration-300 "
               onClick={handleClick}
             >
               Shuffle
             </button>
-          </div>
+          </div> */}
         </div>
-        {fetchError && (
+        {/* {fetchError && (
           <p className="text-red-500 text-sm break-words whitespace-normal flex justify-center items-center text-center">
             {" "}
             {`Sorry,Please try again later.${fetchErrorLog}`}
           </p>
-        )}
+        )} */}
       </div>
       {/* </div> */}
     </>
--- frontend/src/components/TopNav.tsx
+++ frontend/src/components/TopNav.tsx
@@ -3,9 +3,9 @@ function TopNav() {
     <>
       <div className="navbar bg-custom-secondary p-5 flex justify-between">
         <div className="justify-start">
-          <a  href="https://salt.dev/" target="_blank" rel="noopener noreferrer" className="btn btn-ghost text-xl text-white">{"</salt>"}</a>
+          <a  href="https://salt.dev/" target="_blank" rel="noopener noreferrer" className=" bg-custom-secondary border-none  text-xl text-white"> <img src="https://salt.dev/wp-content/uploads/2024/02/salt-logo-light.svg" className="p-1"/></a>
         </div>
-        <div><p className="text-2xl text-white font-bold">Prompt Master</p></div>
+        <div><p className="text-4xl text-white font-bold">Prompt Master</p></div>
         <div className="">
           <a href="https://salt.dev/contact/" target="_blank" rel="noopener noreferrer"><button className="btn bg-white hover:bg-custom-tertiary hover:text-white hover:border-custom-tertiary hover:scale-x-105 ">
             Contact Us
