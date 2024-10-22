43441f886580046b39d47331329756d317ebeda0
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-10-22 03:44:04 +0200

    Prop lifting ImageUrl

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,19 @@ import ScoreCard from "./components/ScoreCard";
 import TargetImage from "./components/TargetImage";
 import TopNav from "./components/TopNav";
 import LeaderBoard from "./components/LeaderBoard";
+import targetImage from "./assets/targetImage";
 
 function App() {
-  const [targetImageUrl, setTargetImageUrl] = useState<string>("https://images.ctfassets.net/kftzwdyauwt9/1LhyQ3RdyPfvqqHJxEfcYe/78e9693b3e5e9ec1711c7a8e69a97e4e/dalle_3_cookie.jpg?w=1920&q=90&fm=webp");
+  const [targetImageUrl, setTargetImageUrl] = useState<string>(
+    targetImage.image
+  );
   // const [imageUrl, setImageUrl] = useState<string>("");
-  const [score, setScore] = useState<string>('');
+  const [score, setScore] = useState<string>("");
   const [playerName, setPlayerName] = useState<string>("");
-  const [playerPhone, setPlayerPhone] = useState('');
+  const [playerPhone, setPlayerPhone] = useState("");
   const [userInfoEntered, setUserInfoEntered] = useState(false);
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [prompt, setPrompt] = useState<string>("");
   return (
     <>
       <TopNav />
@@ -27,33 +31,44 @@ function App() {
         <h1 className="text-6xl font-bold text-white">Prompt Master</h1>
       </div> */}
       {/* <GameHeader/> */}
-      <div className="flex justify-evenly  bg-custom-tertiary p-6">
-      <TargetImage targetImageUrl ={targetImageUrl} setTargetImageUrl={setTargetImageUrl}/>
-      {score && <ScoreCard 
-                  playerScore={score} 
-                  playerName={playerName} 
-                  setPlayerName={setPlayerName}
-                  setScore={setScore} 
-                  setUserInfoEntered={setUserInfoEntered} 
-                  playerPhone={playerPhone} 
-                  setPlayerPhone={setPlayerPhone}
-                  setImageUrl={setImageUrl} /> }
-      <ImageGenerator 
-        setScore={setScore} 
-        targetImageUrl={targetImageUrl} 
-        playerName={playerName} 
-        setPlayerName={setPlayerName} 
-        playerPhone={playerPhone} 
-        setPlayerPhone={setPlayerPhone} 
-        userInfoEntered={userInfoEntered} 
-        setUserInfoEntered={setUserInfoEntered}
-        imageUrl={imageUrl} 
-        setImageUrl={setImageUrl} />
-
+      <div className="flex justify-around bg-custom-tertiary p-6">
+        <TargetImage
+          targetImageUrl={targetImageUrl}
+          setTargetImageUrl={setTargetImageUrl}
+        />
+        {score && (
+          <ScoreCard
+            playerScore={score}
+            playerName={playerName}
+            setPlayerName={setPlayerName}
+            setScore={setScore}
+            setUserInfoEntered={setUserInfoEntered}
+            playerPhone={playerPhone}
+            setPlayerPhone={setPlayerPhone}
+            imageUrl={imageUrl}
+            setImageUrl={setImageUrl}
+            prompt={prompt}
+            setPrompt={setPrompt}
+          />
+        )}
+        <ImageGenerator
+          setScore={setScore}
+          targetImageUrl={targetImageUrl}
+          playerName={playerName}
+          setPlayerName={setPlayerName}
+          playerPhone={playerPhone}
+          setPlayerPhone={setPlayerPhone}
+          userInfoEntered={userInfoEntered}
+          setUserInfoEntered={setUserInfoEntered}
+          imageUrl={imageUrl}
+          setImageUrl={setImageUrl}
+          prompt={prompt}
+          setPrompt={setPrompt}
+        />
       </div>
       <LeaderBoard />
-      <ContactForm/>
-      <Footer/>
+      <ContactForm />
+      <Footer />
     </>
   );
 }
